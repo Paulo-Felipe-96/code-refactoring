43244b02abfb17ccb494807ff30ef94c64f75d87
Paulo-Felipe-96 <paulofelipe0896@gmail.com>
2022-05-30 12:29:37 -0300

    feat: add comment to console data

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 
-const consoleFetchedData = (data) => {
-  console.log(data);
+const consoleFetchedData = (data) =>
+  //console.log(data);
   data.forEach((element) => {
     console.log("Nome:", element.name);
     console.log("Idade média:", element.age);
     console.log("Quantidade de registros", element.count);
     console.log("-----------------------");
   });
-};
 
 async function fetchAgifyData(country) {
   const baseUrl = `https://api.agify.io/`;
