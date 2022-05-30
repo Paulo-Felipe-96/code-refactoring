d6b82d806a5ee204154b42e8ce934f3f1c83bdb8
Paulo-Felipe-96 <paulofelipe0896@gmail.com>
2022-05-30 12:24:08 -0300

    feat: first refactoring with node-fetch

--- index.js
+++ index.js
@@ -1,60 +1,25 @@
-const https = require("https");
+import fetch from "node-fetch";
 
-let getResponse = "";
-
-const doRequest = (country) => {
-  if (country) {
-    https
-      .get(
-        "https://api.agify.io/?name[]=victor&name[]=milson&name[]=gabriela&name[]=patricia&name[]=wagner&name[]=brenda&country_id=" +
-          country,
-        (res) => {
-          if (res.statusCode === 200) {
-            res.on("data", (d) => {
-              getResponse += d;
-            });
-
-            res.on("end", () => {
-              const json = JSON.parse(getResponse);
-              for (let index = 0; index < json.length; index++) {
-                console.log("Nome:", json[index].name);
-                console.log("Idade média:", json[index].age);
-                console.log("Quantidade de registros", json[index].count);
-                console.log("-----------------------");
-              }
-            });
-          }
-        }
-      )
-      .on("error", (e) => {
-        console.error(e);
-      });
-  } else {
-    https
-      .get(
-        "https://api.agify.io/?name[]=victor&name[]=milson&name[]=gabriela&name[]=patricia&name[]=wagner&name[]=brenda",
-        (res) => {
-          if (res.statusCode === 200) {
-            res.on("data", (d) => {
-              getResponse += d;
-            });
-
-            res.on("end", () => {
-              const json = JSON.parse(getResponse);
-              for (let index = 0; index < json.length; index++) {
-                console.log("Nome:", json[index].name);
-                console.log("Idade média:", json[index].age);
-                console.log("Quantidade de registros", json[index].count);
-                console.log("-----------------------");
-              }
-            });
-          }
-        }
-      )
-      .on("error", (e) => {
-        console.error(e);
-      });
-  }
+const consoleFetchedData = (data) => {
+  data.forEach((element) => {
+    console.log("Nome:", element.name);
+    console.log("Idade média:", element.age);
+    console.log("Quantidade de registros", element.count);
+    console.log("-----------------------");
+  });
 };
 
-doRequest("BR");
+async function fetchAgifyData(country) {
+  const baseUrl = `https://api.agify.io/?name[]=victor&name[]=milson&name[]=gabriela&name[]=patricia&name[]=wagner&name[]=brenda`;
+  const completeUrl = country ? baseUrl + `&country_id=${country}` : baseUrl;
+
+  try {
+    const request = await fetch(completeUrl);
+    return request.json();
+  } catch (e) {
+    console.warn(`Error: ${e}`);
+  }
+}
+
+fetchAgifyData("CA").then((data) => consoleFetchedData(data));
+fetchAgifyData().then((data) => consoleFetchedData(data));
